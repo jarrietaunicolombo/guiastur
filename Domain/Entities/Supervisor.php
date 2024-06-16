931bfa03a5ec734304a4d211ca11367c30deb306
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2024-06-15 21:35:48 -0500

    resilviendo comflitos

--- Domain/Entities/Supervisor.php
+++ Domain/Entities/Supervisor.php
@@ -2,10 +2,7 @@
 require_once $_SERVER["DOCUMENT_ROOT"]."/guiastur/Infrastructure/Libs/Orm/Config.php";
 
 class Supervisor  extends  ActiveRecord\Model {
-<<<<<<< Updated upstream
-    public static $table_name = "Supervisores";
-=======
->>>>>>> Stashed changes
+
     public static $primary_key = "cedula";
     public static $belongs_to = array(array("usuario"));
     public static $has_many = array(array("atenciones"));
