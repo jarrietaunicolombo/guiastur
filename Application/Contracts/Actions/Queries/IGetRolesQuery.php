019d8c3ea9fdd8c196603ad6e2658ee70a7caead
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2024-07-06 00:47:14 -0500

    Logica de backend para obtener los Roles

---
+++ Application/Contracts/Actions/Queries/IGetRolesQuery.php
@@ -0,0 +1,7 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "guiastur/Application/UseCases/GetRoles/Dto/GetRolesResponse.php";
+
+interface IGetRolesQuery{
+    public function handler() : GetRolesResponse;
+    
+}
