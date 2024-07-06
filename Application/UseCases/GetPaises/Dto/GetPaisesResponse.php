358f26a8061979ded364c73519ab9a03dd3c2d15
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2024-07-06 00:45:11 -0500

    Logica de backen para obtener los paises

---
+++ Application/UseCases/GetPaises/Dto/GetPaisesResponse.php
@@ -0,0 +1,46 @@
+<?php
+
+class GetPaisesResponse{
+    private $paises;
+    public function __construct(array $paises){
+        if ($paises === NULL) {
+            $paises = array();
+        }
+        $this->paises = $paises;
+    }
+    public function getPaises() : array{
+        return $this->paises;
+    }
+}
+
+
+class PaisResponse{
+    private $id;
+    private $nombre;
+    private $bandera;
+
+    public function __construct(int $id, string $nombre, string $bandera = null) {
+        if($id === NULL || $id < 1) {
+            throw new \InvalidArgumentException("El Id es requerido para Obtener los Paises");
+        }
+        if($nombre === NULL || empty(trim($nombre))) {
+            throw new \InvalidArgumentException("El Nombre es requerido para Obtener los Paises");
+        }
+
+        $this->id = $id;
+        $this->nombre = $nombre;    
+        $this->bandera = $bandera;  
+    }
+
+    public function getId(): int { 
+        return $this->id;
+    }
+
+    public function getNombre(): string {   
+        return $this->nombre;
+    }
+
+    public function getBandera() {
+        return $this->bandera;
+    }
+}
