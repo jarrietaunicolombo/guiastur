5139180c93ba455eb38af8caa7f18ee66576a761
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2024-07-25 20:47:07 -0500

    Cambios en la consulta de login para agregar unas restricciones de acceso

--- Application/Actions/Queries/LoginQueryHandler.php
+++ Application/Actions/Queries/LoginQueryHandler.php
@@ -1,8 +1,9 @@
-<?php
+php/<?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "guiastur/Application/UseCases/Login/Dto/LoginRequest.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "guiastur/Application/UseCases/Login/Dto/LoginResponse.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "guiastur/Application/Contracts/Actions/Queries/ILoginQuery.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "guiastur/Application/Contracts/Repositories/IUsuarioRepository.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "guiastur/Domain/Constants/UsuarioStatus.php";
 
 class LoginQueryHandler implements ILoginQuery{
 
@@ -16,15 +17,25 @@ class LoginQueryHandler implements ILoginQuery{
     public function handler(LoginRequest $request) : LoginResponse{
     
         $usuario = $this->repository->findByEmail($request->getEmail());
-        if($usuario->password != $request->getPassword()){
+
+        if($usuario->estado == UsuarioStatus::CREATED){
+            throw new Exception("Aun no ha activado su cuenta, revise su email");
+        }
+
+        if($usuario->estado != UsuarioStatus::ACTIVATED){
+            throw new Exception("Acceso denegado");
+        }
+
+        if($usuario->password != $request->getPassword() ){
             throw new Exception("Datos de acceso incorrectos");
         }
+
         return new LoginResponse($usuario->id
                                     , $usuario->email
                                     , $usuario->nombre
                                     , $usuario->estado
                                     , $usuario->rol->nombre
-                                    , $usuario->guia_o_supervisor_id+"");
+                                    , strval($usuario->guia_o_supervisor_id));
     }
 }
 
